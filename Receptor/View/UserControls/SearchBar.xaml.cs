db582914fd5a7981f41b593c823dc821374d3504
kubaadamcik <jakubadamcik11@gmail.com>
2025-04-11 11:48:20 +0200

    Initial commit

---
+++ Receptor/View/UserControls/SearchBar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Receptor.View.UserControls;
+
+public partial class SearchBar : UserControl
+{
+    public SearchBar()
+    {
+        InitializeComponent();
+    }
+
+    private void HidePlaceholder(object sender, RoutedEventArgs e)
+    {
+        TextBox textBox = sender as TextBox;
+
+        SearchLabel.Content = String.Empty;
+
+    }
+
+    private void ShowPlaceholder(object sender, RoutedEventArgs e)
+    {
+        TextBox textBox = sender as TextBox;
+
+        if (string.IsNullOrEmpty(textBox.Text)) SearchLabel.Content = "Vyhledat recepty...";
+    }
+}
