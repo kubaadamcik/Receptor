c533b6c7869daf25e5fbf6c958b0989dcbaecd48
kubaadamcik <jakubadamcik11@gmail.com>
2025-04-15 09:56:32 +0200

    Něco

--- Receptor/obj/Debug/net9.0-windows/App.g.cs
+++ Receptor/obj/Debug/net9.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace Receptor {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace Receptor {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
         public static void Main() {
             Receptor.App app = new Receptor.App();
             app.InitializeComponent();
