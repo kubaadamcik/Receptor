0d13c29d23ab13f80f15e7780fd4ae4d7983447d
kubaadamcik <jakubadamcik11@gmail.com>
2025-04-25 12:30:36 +0200

    quick

---
+++ Receptor/View/UserControls/AddRecipe.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Receptor.View.UserControls;
+
+public partial class AddRecipe : UserControl
+{
+    public AddRecipe()
+    {
+        InitializeComponent();
+    }
+}
